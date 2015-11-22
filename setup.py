bf5878bafb33e877fcb42e245b1db82985c5b849
JohnHouwer <john.houwer@gmail.com>
2015-11-22 12:17:27 +0100

    Migrate to cgi.Fieldstorage for multipart parsing
    Now uploaded files are identic to the source.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.1.1',
 
     description='picture upload server for the included slideshow viewer',
     long_description=long_description,
@@ -51,7 +51,7 @@ setup(
 
         'Environment :: Console',
         'Environment :: Web Environment',
-       
+
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: MacOS',
@@ -76,10 +76,10 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    #packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    #packages=find_packages(),
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    # packages=find_packages(),
     packages=['pypartypix'],
-    #scripts=["pyparts/pyparty.py"],
+    # scripts=["pyparts/pyparty.py"],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -95,9 +95,9 @@ setup(
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    #extras_require={
+    # extras_require={
     #    'qrcode': ['pyqrcode', 'pypng'],
-    #},
+    # },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
@@ -112,7 +112,7 @@ setup(
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
+    # data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
